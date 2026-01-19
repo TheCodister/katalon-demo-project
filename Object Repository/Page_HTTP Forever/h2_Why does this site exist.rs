04a498f593d911512affc3074bdde409b97576b0
TheKodister <quang.nnguyen@katalon.com>
2026-01-19 11:58:19 +0700

    Commit new changes

---
+++ Object Repository/Page_HTTP Forever/h2_Why does this site exist.rs
@@ -0,0 +1,98 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h2_Why does this site exist</name>
+   <tag></tag>
+   <elementGuidId>60d2d762-4b48-48fc-a17b-eb97783451b3</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='HTTP FOREVER'])[1]/following::h2[1]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>header.major > h2</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=heading[name=&quot;Why does this site exist?&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h2</value>
+      <webElementGuid>8f1feb1e-1f53-4c94-810e-2aae46638b98</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Why does this site exist?</value>
+      <webElementGuid>751e3143-325f-434e-8aa8-21fabe46db26</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[@class=&quot;global wide normal&quot;]/body[@class=&quot;landing&quot;]/div[@class=&quot;wrapper style1&quot;]/section[@class=&quot;container&quot;]/header[@class=&quot;major&quot;]/h2[1]</value>
+      <webElementGuid>f8d05c09-2b4e-4877-bf6a-a37ef7226db3</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='HTTP FOREVER'])[1]/following::h2[1]</value>
+      <webElementGuid>f900a9ea-92c6-4bed-8af8-1039b83c555a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='How does it work?'])[1]/preceding::h2[1]</value>
+      <webElementGuid>07b64023-0608-45b4-83da-8030dbaaaaa2</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='never'])[1]/preceding::h2[2]</value>
+      <webElementGuid>5f088dfb-b03b-4dda-b691-689190b47ea4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Why does this site exist?']/parent::*</value>
+      <webElementGuid>a5f946cd-f77b-408a-985c-b7e973cfff2c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//header/h2</value>
+      <webElementGuid>a701df85-8051-44fe-9fc9-7c0e98ab4232</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//h2[(text() = 'Why does this site exist?' or . = 'Why does this site exist?')]</value>
+      <webElementGuid>b9eccab2-bec9-481a-bfb4-3f4aa5a43df9</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
